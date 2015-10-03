e7ec8b8578511fb89878ad8ae8888b7f80bdb521
wegamekinglc <scrappedprince.li@gmail.com>
2015-10-03 10:38:03 +0800

    change the examples

--- AlgoTrading/examples/MovingAverageCrossStrategy.py
+++ AlgoTrading/examples/MovingAverageCrossStrategy.py
@@ -15,15 +15,12 @@ class MovingAverageCrossStrategy(Strategy):
     def __init__(self):
         self.short_sma = MA(10, 'close')
         self.long_sma = MA(120, 'close')
-        pass
 
     def handle_data(self):
-        short_sma = self.short_sma.value
-        long_sma = self.long_sma.value
         for s in self.universe:
-            if short_sma[s] > long_sma[s] and self.secPos[s] == 0:
+            if self.short_sma[s] > self.long_sma[s] and self.secPos[s] == 0:
                 self.order(s, 1, quantity=1000)
-            elif short_sma[s] < long_sma[s] and self.secPos[s] != 0:
+            elif self.short_sma[s] < self.long_sma[s] and self.secPos[s] != 0:
                 self.order(s, -1, quantity=1000)
 
 
