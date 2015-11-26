0eb77109d6afbacf3b6ad946bb326c004b05c177
Cheng Li <scrappedprince.li@gmail.com>
2015-11-26 09:46:39 +0800

    change content of DataYes strategy

--- AlgoTrading/examples/DataYesBasedStrategy.py
+++ AlgoTrading/examples/DataYesBasedStrategy.py
@@ -1,80 +1,41 @@
 # -*- coding: utf-8 -*-
 u"""
-Created on 2015-9-22
+Created on 2015-9-23
 
 @author: cheng.li
 """
 
 import datetime as dt
-import pandas as pd
 
 from AlgoTrading.Strategy.Strategy import Strategy
 from AlgoTrading.Backtest import strategyRunner
-from AlgoTrading.Backtest import DataSource
-from PyFin.api import CLOSE
-from PyFin.api import OPEN
-from PyFin.api import LOW
-from PyFin.api import HIGH
-from PyFin.api import nthWeekDay
-from PyFin.api import advanceDateByCalendar
+from AlgoTrading.Enums import DataSource
+from AlgoTrading.Data import set_universe
+from PyFin.api import MA
+from PyFin.api import MAX
+from PyFin.api import MIN
 
 
 class MovingAverageCrossStrategy(Strategy):
     def __init__(self):
-
-        indexes = ['000016.zicn', '000300.zicn', '000905.zicn']
-
-        self.closes = CLOSE()
-        self.preCloses = CLOSE().shift(1)
-        self.openes = OPEN()
-        self.lowes = LOW()
-        self.highes = HIGH()
-
-        # define indexes signal
-        self.closeDLastClose = self.closes / self.preCloses
-        self.closeDOpen = self.closes / self.openes
-        self.closeDLow = self.closes / self.lowes
-        self.oneMinusHigDClose = 1. - self.highes / self.closes
-
-        self.indexSignals = [self.closeDLastClose, self.closeDOpen, self.closeDLow, self.oneMinusHigDClose]
-
-        # index weight to calculate final score
-        self.indexWeights = pd.Series([1., -2., 1.], index=['000016.zicn', '000300.zicn', '000905.zicn'])
+        filtering = (MAX(10, 'close') / MIN(10, 'close')) >= 1.02
+        indicator = MA(10, 'close') - MA(120, 'close')
+        self.signal = indicator[filtering]
 
     def handle_data(self):
-
-        # calculate the index score
-        elects = 0
-        for signal in self.indexSignals:
-            value = signal['000016.zicn', '000300.zicn', '000905.zicn'].dot(self.indexWeights)
-            if value > 0.:
-                elects += 1
-
-        # find the right contracts to trade
-        current_time = self.current_datetime
-
-        year = current_time.year
-        month = current_time.month
-
-        delDay = nthWeekDay(3, 6, month, year)
-        changeContractDay = advanceDateByCalendar('China.SSE', delDay, '-3b')
-        print(u"换仓日:{0} 交割日：{1}".format(changeContractDay, delDay))
+        for s in self.universe:
+            amount = self.avaliableForSale(s)
+            if self.signal[s] > 0. and self.secPos[s] == 0:
+                self.order(s, 1, quantity=200)
+            if self.signal[s] < 0. and amount != 0:
+                self.order(s, -1, quantity=200)
 
 
 def run_example():
-    indexes = ['000016.zicn', '000300.zicn', '000905.zicn']
-
-    # futures to trade
-    ihs = ['ih15%02d' % i for i in range(6, 12)]
-    ifs = ['if15%02d' % i for i in range(6, 12)]
-    ics = ['ic15%02d' % i for i in range(6, 12)]
-
-    futures = ihs + ifs + ics
-
-    universe = indexes + futures
+    universe = set_universe('000300.zicn')
     initialCapital = 100000.0
-    startDate = dt.datetime(2015, 6, 1)
-    endDate = dt.datetime(2015, 11, 1)
+    startDate = dt.datetime(2006, 1, 1)
+    endDate = dt.datetime(2015, 10, 1)
 
     strategyRunner(userStrategy=MovingAverageCrossStrategy,
                    initialCapital=initialCapital,
@@ -82,9 +43,10 @@ def run_example():
                    startDate=startDate,
                    endDate=endDate,
                    dataSource=DataSource.DataYes,
+                   freq=0,
                    benchmark='000300.zicn',
                    saveFile=False,
-                   plot=False)
+                   plot=True)
 
 
 if __name__ == "__main__":
