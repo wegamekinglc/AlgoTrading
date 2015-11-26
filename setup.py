f5c720b616521d80cb8cb920737dc5466b9c80f2
Cheng Li <scrappedprince.li@gmail.com>
2015-11-26 11:26:04 +0800

    updated version number

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ else:
 
 setup(
     name='AlgoTrading',
-    version='0.1.3',
+    version='0.1.4',
     packages=['AlgoTrading',
               'AlgoTrading.Assets',
               'AlgoTrading.Backtest',
