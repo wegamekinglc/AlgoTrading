532698c70081406a8c4db63d1e89bab4f8cb6341
Cheng Li <scrappedprince.li@gmail.com>
2015-12-16 09:36:39 +0800

    made commission and asset definition visiable to user

--- AlgoTrading/api/__init__.py
+++ AlgoTrading/api/__init__.py
@@ -7,14 +7,36 @@ Created on 2015-12-10
 
 from AlgoTrading.Strategy import Strategy
 from AlgoTrading.Backtest import strategyRunner
+
 from AlgoTrading.Enums import PortfolioType
 from AlgoTrading.Enums import DataSource
+
 from AlgoTrading.Env import Settings
 from AlgoTrading.Data import set_universe
+
+# commission types
+from AlgoTrading.Finance import PerShare
+from AlgoTrading.Finance import PerTrade
+from AlgoTrading.Finance import PerValue
+
+# asset types
+from AlgoTrading.Assets import XSHGStock
+from AlgoTrading.Assets import XSHEStock
+from AlgoTrading.Assets import IFFutures
+from AlgoTrading.Assets import ICFutures
+from AlgoTrading.Assets import IHFutures
 
 __all__ = ['Strategy',
            'strategyRunner',
            'PortfolioType',
            'DataSource',
            'Settings',
-           'set_universe']
+           'set_universe',
+           'PerShare',
+           'PerTrade',
+           'PerValue',
+           'XSHGStock',
+           'XSHEStock',
+           'IFFutures',
+           'ICFutures',
+           'IHFutures']
