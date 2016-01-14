d82a5f6fe672ad3f62095b9fa507235fdad166c1
Cheng Li <scrappedprince.li@gmail.com>
2016-01-14 22:13:09 +0800

    added missing file

---
+++ AlgoTrading/Assets/Indexes.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+u"""
+Created on 2015-11-13
+
+@author: cheng.li
+"""
+
+from AlgoTrading.Assets.base import Asset
+from AlgoTrading.Finance.Commission import PerValue
+
+
+class EXIndex(Asset):
+    u"""
+
+    交易所指数
+
+    """
+    lag = 0
+    exchange = "SE"
+    commission = PerValue(buyCost=0.0, sellCost=0.0)
+    multiplier = 1.
+    margin = 0.
+    settle = 1.
+    minimum = 1
+    short = True
