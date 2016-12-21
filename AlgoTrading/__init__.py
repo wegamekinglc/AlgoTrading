9cfe6c2ad3569513f84780269eab9c20eac666c1
Cheng Li <scrappedprince.li@gmail.com>
2016-12-21 17:13:28 +0800

    bump version to 0.2.2

--- AlgoTrading/__init__.py
+++ AlgoTrading/__init__.py
@@ -5,4 +5,4 @@ Created on 2015-7-24
 @author: cheng.li
 """
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ else:
 
 setup(
     name='AlgoTrading',
-    version='0.2.1',
+    version='0.2.2',
     packages=['AlgoTrading',
               'AlgoTrading.Assets',
               'AlgoTrading.Backtest',
